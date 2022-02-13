bdfca0a5fe1fcc36de2fd3f993970a3b7003cff3
Daniel Batista <danielbped@gmail.com>
2022-02-13 16:31:11 -0300

    Carousel

---
+++ src/assets/Carousel-items.jsx
@@ -0,0 +1,7 @@
+const images = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXcRqE6qwn31cjm3jq_YFv0JnMS07Hit3jNQ&usqp=CAU",
+  "https://img.pikbest.com/backgrounds/20210629/blue-cute-baby-store-discount-promotion-propaganda-banner_6038297.jpg!c1024wm0",
+  "https://media.istockphoto.com/vectors/free-shipping-on-all-order-clean-banner-with-product-cardboard-box-vector-id1222834779"
+];
+
+export default images;
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Slider from 'react-slick';
+import items from '../../assets/Carousel-items';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../../style/carousel.css";
 
-const Carousel = () => (
-  <h1>
-    Carousel
-  </h1>
-);
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return (
+    <div className="carousel">
+      <Slider { ...settings }>
+        { items.map((item) => 
+          <div className="carousel__item">
+            <img
+              src={item}
+              alt="carousel"
+              className="carousel__item__image"
+            />
+          </div>)
+        }
+      </Slider>
+    </div>
+  );
+};
 
 export default Carousel;
